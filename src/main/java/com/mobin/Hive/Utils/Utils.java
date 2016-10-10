a4d78cf10b715e12f4e3d147515f28eacfba8b03
MOBIN-F <996833461@qq.com>
2016-10-10 17:23:21 +0800

    优化代码

--- src/main/java/com/mobin/Hive/Utils/Utils.java
+++ src/main/java/com/mobin/Hive/Utils/Utils.java
@@ -20,25 +20,25 @@ public class Utils {
         System.out.println(hiveJSON);
         String urlStr = "http://data.dgchina.com/xingji/Handle/GetData.aspx?model="+hiveJSON;
         URL url;
-        try {
-            url = new URL(urlStr);
-            URLConnection URLconnection = url.openConnection();
-            HttpURLConnection httpConnection = (HttpURLConnection)URLconnection;
-            httpConnection.setDoOutput(true);
-            httpConnection.setChunkedStreamingMode(hiveJSON.getBytes().length);
-            httpConnection.setRequestMethod("POST");
-
-            DataOutputStream out = new DataOutputStream(httpConnection.getOutputStream());
-            out.writeUTF(hiveJSON);
-            out.flush();
-            out.close();
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (ProtocolException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            url = new URL(urlStr);
+//            URLConnection URLconnection = url.openConnection();
+//            HttpURLConnection httpConnection = (HttpURLConnection)URLconnection;
+//            httpConnection.setDoOutput(true);
+//            httpConnection.setChunkedStreamingMode(hiveJSON.getBytes().length);
+//            httpConnection.setRequestMethod("POST");
+//
+//            DataOutputStream out = new DataOutputStream(httpConnection.getOutputStream());
+//            out.writeUTF(hiveJSON);
+//            out.flush();
+//            out.close();
+//        } catch (MalformedURLException e) {
+//            e.printStackTrace();
+//        } catch (ProtocolException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
     }
 
     //计算两个年份相差多少个月
