f56b7b23eb428bfa73a53f458d39f625a6d787e3
MOBIN-F <996833461@qq.com>
2016-10-14 09:06:48 +0800

    声量来源

--- src/main/java/com/mobin/Hive/Source.java
+++ src/main/java/com/mobin/Hive/Source.java
@@ -54,29 +54,29 @@ public class Source {
         }else if(Num == 2 || Num == 6){  //三天线
             hql = "WITH wtmp AS (SELECT rowkey,to_date(s6) AS time,cx FROM raws WHERE (s3a=='1' OR s9!='1') AND  (mth>=? AND mth<=?) AND to_date(s6)>=? AND to_date(s6)<=?), "+
             "cx_tmp AS "+
-            "(SELECT s3a,mt,sum(c) over(PARTITION BY s3a ORDER BY mt DESC rows between CURRENT row and "+Num+" following) AS cx_per FROM "+
+            "(SELECT s3a,mt,sum(c) over(PARTITION BY s3a ORDER BY mt DESC rows between CURRENT row and 2 following) AS cx_per FROM "+
                     "(SELECT count(1) AS c,concat_ws(',',model,time) AS mt,s3a FROM "+
                                     "(SELECT sid,s3a,cbrdint3 AS model FROM bca "+
                                             "LATERAL VIEW explode(brdint3) tbrdint3 AS cbrdint3 "+
-                                            "WHERE (s3a=='1' OR s9!='1') AND mth>=? AND mth<=? AND s3a!=4 AND cbrdint3!='') t1 "+
+                                            "WHERE (s3a==',' OR s9!=',') AND mth>=? AND mth<=? AND s3a!=4 AND cbrdint3!='') t1 "+
                             "JOIN wtmp ON base64(t1.sid)=base64(substr(wtmp.rowkey,0,16)) GROUP BY model,time,s3a) "+
             "t2) "+
-            "SELECT t5.mt,s3a, "+
-                    "(cx_per/brd3_cot) AS pre FROM "+
-                    "(SELECT t5.mt,cot,(sum(cot) over(ORDER BY mt DESC rows between CURRENT row and "+Num+" following)) AS brd3_cot,model,time  FROM "+
-                    "(SELECT concat_ws(',',model,time) AS mt,count(1) AS cot,model,time  FROM "+
+            "SELECT model,time,s3a,brd3_cot AS brd3_cot, "+
+            "(cx_per/brdint3_cot) AS pre FROM "+
+                    "(SELECT model,time,t5.mt,cot,(sum(cot) over(ORDER BY mt DESC rows between CURRENT row and 2 following)) AS brdint3_cot FROM "+
+                    "(SELECT concat_ws(',',model,time) AS mt,count(1) AS cot,model,time FROM "+
                                     "(SELECT sid,cbrdint3 AS model FROM bca "+
-                                            "LATERAL VIEW explode(brdint3) tbrdint3 AS cbrdint3 WHERE (s3a=='1' OR s9!='1') AND (mth>=? AND mth<=?) AND cbrdint3!='') t3 "+
+                                            "LATERAL VIEW explode(brdint3) tbrdint3 AS cbrdint3 WHERE (s3a==',' OR s9!=',') AND (mth>=? AND mth<=?) AND cbrdint3!='') t3 "+
                             "JOIN wtmp "+
                             "ON base64(t3.sid)=base64(substr(wtmp.rowkey,0,16)) "+
-                            "GROUP BY model,time) t5) t5 "+
+                            "GROUP BY model,time) t5)t5 "+
             "JOIN cx_tmp ON (cx_tmp.mt=t5.mt) ";
         }else {  //月线
             hql = "WITH cx_tmp AS "+
             "(SELECT s3a,count(1) AS c,concat_ws(',',cbrdint3,mth) AS mt "+
             "FROM bca LATERAL VIEW explode(brdint3) tbrdint3 AS cbrdint3 "+
             "WHERE (s3a=='1' OR s9!='1') AND  mth>=? AND mth<=? GROUP BY cbrdint3,mth,s3a) "+
-            "SELECT model,time,brd3_cot,c/brd3_cot AS per FROM "+
+            "SELECT model,time,brd3_cot,c/brd3_cot AS per,s3a FROM "+
             "(SELECT concat_ws(',',model,time) AS mt,count(1) AS brd3_cot,model,time "+
             "FROM "+
                     "(SELECT sid,cbrdint3 AS model,mth AS time FROM bca "+
@@ -111,6 +111,7 @@ public class Source {
                 hb.setModel(rs.getString("model"));
                 hb.setTime(rs.getString("time"));
                 hb.setBrd3_cot(rs.getInt("brd3_cot"));
+                hb.setS3a_attimg(rs.getString("s3a"));
                 hb.setPer(rs.getDouble("per"));
                 hb.setType(type);
                 hiveBean.add(hb);
